207fb78e969e69c21d629fde84819b95eb9f524d
minhnguyen <ntminh611@gmail.com>
2022-08-05 00:17:22 +0700

    fixed

--- main.go
+++ main.go
@@ -4,20 +4,10 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"github.com/ethereum/go-ethereum/crypto"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
 	"strings"
-	"time"
 )
-
-var eth map[string]string
-var btc map[string]string
-var wallet map[string]string
-var result map[string]string
 
 type Wallet struct {
 	publicKey, privateKey string
@@ -32,7 +22,7 @@ func main() {
 		public := strings.ToLower(ethWallet.publicKey)
 		if isNice(public) {
 			wallet[public] = ethWallet.privateKey
-			writeFile("wallet")
+			writeWallets(wallet, "wallet")
 		}
 	}
 }
@@ -76,49 +66,11 @@ func generateEthWallet() Wallet {
 	return Wallet{strings.ToLower(address), privateKey}
 }
 
-func getEthWallets(page int) {
-	fmt.Println(page)
-	res, err := http.Get(fmt.Sprintf("https://etherscan.io/accounts/%d?ps=100", page))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal("Error loading HTTP response body. ", err)
-	}
-
-	doc.Find(`.table tbody tr`).Each(func(i int, selection *goquery.Selection) {
-		walletAddr := selection.Find(`td`).Eq(1).Text()
-		eth[strings.ToLower(walletAddr)] = "1"
-	})
-
-	_, bool := doc.Find(`.pagination [title='Go to Next'] a`).First().Attr("href")
-	if bool {
-		time.Sleep(time.Second)
-		getEthWallets(page + 1)
-	}
-}
-
-func writeFile(name string) {
-	var jsonData []byte
-	var err error
-	switch name {
-	case "eth":
-		jsonData, err = json.Marshal(eth)
-	case "btc":
-		jsonData, err = json.Marshal(btc)
-	case "wallet":
-		jsonData, err = json.Marshal(wallet)
-	case "result":
-		jsonData, err = json.Marshal(result)
-	}
-
+func writeWallets(data map[string]string, name string) {
+	jsonData, err := json.Marshal(data)
 	if err != nil {
 		panic(err)
 	}
-
 	jsonFile, err := os.Create(fmt.Sprintf("./%s.json", name))
 
 	if err != nil {
@@ -128,26 +80,4 @@ func writeFile(name string) {
 
 	jsonFile.Write(jsonData)
 	jsonFile.Close()
-}
-
-func readFile(name string) {
-	jsonFile, err := os.Open(fmt.Sprintf("./%s.json", name))
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(fmt.Sprintf("Successfully Opened %s.json", name))
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	switch name {
-	case "eth":
-		json.Unmarshal(byteValue, &eth)
-	case "btc":
-		json.Unmarshal(byteValue, &btc)
-	case "wallet":
-		json.Unmarshal(byteValue, &wallet)
-	case "result":
-		json.Unmarshal(byteValue, &result)
-	}
 }
